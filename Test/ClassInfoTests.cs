7f20d2a0288cc94314b8912fc030bf26f2d53e95
wli <weihua_li@denso-diam.com>
2017-05-25 12:17:54 -0400

    Init

---
+++ Test/ClassInfoTests.cs
@@ -0,0 +1,93 @@
+﻿using HelperExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelperExpressionsTest
+{
+    [TestClass]
+    public class ClassInfoTests
+    {
+        private int _loadTestLoopCount = 100000000;
+        private ClassForTest _classForTest;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _classForTest = new ClassForTest { A = 101, B = "qwertyuiop", C = 3.1415926f, D = DateTime.Today };
+        }
+
+        [TestMethod]
+        public void GetPropertyFuncTestInt()
+        {
+            var info = new ClassInfos<ClassForTest>();
+            var method = info.GetPropertyFunc<int>("A");
+            var actual = (int)method(_classForTest);
+            Assert.AreEqual(101, actual);
+        }
+
+        [TestMethod]
+        public void GetPropertyGetMethodInfoLoadTestInt()
+        {
+            var info = new ClassInfos<ClassForTest>();
+            var method = info.GetPropertyGetMethodInfo("A");
+            for (int i = 0; i < _loadTestLoopCount; i++)
+            {
+                var actual = (int)method.Invoke(_classForTest, null);
+                if (actual != 101)
+                    throw new AssertFailedException($"Result was wrong {actual}");
+            }
+            //Assert.AreEqual(101, actual);
+        }
+
+        //[TestMethod]
+        //public void GetPropertyDelegateLoadTestInt()
+        //{
+        //    var info = new ClassInfos<ClassForTest>();
+        //    var method = info.GetPropertyDelegate("A");
+        //    for (int i = 0; i < _loadTestLoopCount; i++)
+        //    {
+        //        var actual = (int)method(_classForTest);
+        //        if (actual != 101)
+        //            throw new AssertFailedException($"Result was wrong {actual}");
+        //    }
+        //    //Assert.AreEqual(101, actual);
+        //}
+        [TestMethod]
+        public void GetPropertyLoadTestInt()
+        {
+            for (int i = 0; i < _loadTestLoopCount; i++)
+            {
+                var actual = _classForTest.A;
+                if (actual != 101)
+                    throw new AssertFailedException($"Result was wrong {actual}");
+            }
+            //Assert.AreEqual(101, actual);
+        }
+
+        [TestMethod]
+        public void GetPropertyFuncLoadTestInt()
+        {
+            var info = new ClassInfos<ClassForTest>();
+            var method = info.GetPropertyFunc<int>("A");
+            for (int i = 0; i < _loadTestLoopCount; i++)
+            {
+                var actual = (int)method(_classForTest);
+                if (actual != 101)
+                    throw new AssertFailedException($"Result was wrong {actual}");
+            }
+            //Assert.AreEqual(101, actual);
+        }
+
+    }
+
+
+    public class ClassForTest
+    {
+        public int A { get; set; }
+        public string B { get; set; }
+        public float C { get; set; }
+        public DateTime D { get; set; }
+    }
+}
---
+++ src/ClassInfos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace HelperExpressions
+{
+    public class ClassInfos<T>
+    {
+        private static readonly Type _type = typeof(T);
+        public Func<T, Tout> GetPropertyFunc<Tout> (string propertyName) 
+        {
+            ParameterExpression arg = Expression.Parameter(_type);
+            Expression expr = Expression.Property(arg, propertyName);
+            var propResolver = Expression.Lambda<Func<T, Tout>>(expr, arg).Compile();
+            return propResolver;
+        }
+
+        public MethodInfo GetPropertyGetMethodInfo(string propertyName)
+        {
+            return _type.GetRuntimeProperty(propertyName).GetMethod;
+        }
+
+        public Delegate GetPropertyDelegate(string propertyName)
+        {
+            ParameterExpression arg = Expression.Parameter(_type);
+            Expression expr = Expression.Property(arg, propertyName);
+            var propResolver = Expression.Lambda(expr, arg).Compile();
+            return propResolver;
+        }
+    }
+}
